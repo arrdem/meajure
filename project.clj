8053d7e358c7fbcf0757396d3cec06566cf5196f
arrdem <me@arrdem.com>
2014-04-17 21:54:15 -0500

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject meajure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject me.arrdem/meajure "0.1.0-SNAPSHOT"
+  :description "A Clojure math with units library"
+  :url "http://github.com/arrdem/meajure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/algo.generic "0.1.2"]])
