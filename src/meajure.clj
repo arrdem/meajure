c7d0f8d2b573b96597d20035081818c65f64304a
arrdem <me@arrdem.com>
2014-04-18 14:15:50 -0500

    Rework imports to resolve load errors

--- src/meajure.clj
+++ src/meajure.clj
@@ -1,17 +1,17 @@
 (ns ^{:author "Reid McKenzie"
       :doc "Math with units
-	    This library implements algo.generic math operations
-	    across values which have attached symbolic units. This
-	    library makes no attempt to provide transition functions
-	    or automatic conversion, it simply provides unit tracking
-	    through algebraic operations."}
+            This library implements algo.generic math operations
+            across values which have attached symbolic units. This
+            library makes no attempt to provide transition functions
+            or automatic conversion, it simply provides unit tracking
+            through algebraic operations."}
 
   meajure
   (:require [clojure.algo.generic
-	     [arithmetic :refer :all]
-	     [comparison :refer :all]
-	     [math-functions :refer :all]])
-  (:refer-clojure :exclude [- + / * zero? pos? neg?]))
+             [arithmetic :refer :all]
+             [comparison :as c]
+             [math-functions :refer :all]])
+  (:refer-clojure :exclude [- + / *]))
 
 
 ;; Math with units
@@ -25,14 +25,14 @@
   [v & kvs]
   {:pre [(even? (count kvs))]}
   (->UnitValue v (->> kvs
-		      (partition 2)
-		      (reduce (partial apply assoc) {}))))
+                      (partition 2)
+                      (reduce (partial apply assoc) {}))))
 
 (defn elimimate-zeros [{:keys [units] :as v}]
   (reduce (fn [x k]
-	    (if (zero? (get units k))
-	      (update-in x [:units] dissoc k) x))
-	  v (keys units)))
+            (if (zero? (get units k))
+              (update-in x [:units] dissoc k) x))
+          v (keys units)))
 
 (defn scalar? [x]
   (and (number? (:val x))
@@ -41,7 +41,7 @@
 (defn has-units? [x]
   (or (instance? UnitValue x)
       (and (map? x)
-	   (:val x))))
+           (:val x))))
 
 (defn to-scalar
   [n]
@@ -55,7 +55,7 @@
 (defn units-equal?
   [x y]
   {:pre [(has-units? x)
-	 (has-units? y)]}
+         (has-units? y)]}
   (= (:units x)
      (:units y)))
 
@@ -99,10 +99,10 @@
   [x y]
   (-> x
       (assoc
-	  :val (* (:val x) (:val y))
-	  :units (merge-with +
-			     (:units x)
-			     (:units y)))
+          :val (* (:val x) (:val y))
+          :units (merge-with +
+                             (:units x)
+                             (:units y)))
       simplify))
 
 (defmethod * [java.lang.Number meajure.UnitValue]
@@ -118,8 +118,8 @@
   [x y]
   (-> x
       (assoc
-	  :val (/ (:val x) (:val y))
-	  :units (merge-with - (:units x) (:units y)))
+          :val (/ (:val x) (:val y))
+          :units (merge-with - (:units x) (:units y)))
       simplify))
 
 (defmethod / [java.lang.Number meajure.UnitValue]
@@ -159,52 +159,52 @@
   (-> x
       (update-in [:val] pow n)
       (update-in [:units]
-		 (fn [mapping]
-		   (->> (for [[k v] mapping]
-			  [k (* v n)])
-			(into {}))))))
+                 (fn [mapping]
+                   (->> (for [[k v] mapping]
+                          [k (* v n)])
+                        (into {}))))))
 
 (defmethod sqrt meajure.UnitValue [x]
   (-> x
       (update-in [:val] sqrt)
       (update-in [:units]
-		 (fn [mapping]
-		   (->> (for [[k v] mapping]
-			  [k (/ v 2)])
-			(into {}))))))
+                 (fn [mapping]
+                   (->> (for [[k v] mapping]
+                          [k (/ v 2)])
+                        (into {}))))))
 
 ;; Implement comparisons
 ;;--------------------------------------------------------------------
-(defmethod pos? meajure.UnitValue [x]
-  (pos? (to-scalar x)))
+(defmethod c/pos? meajure.UnitValue [x]
+  (c/pos? (to-scalar x)))
 
-(defmethod neg? meajure.UnitValue [x]
-  (neg? (to-scalar x)))
+(defmethod c/neg? meajure.UnitValue [x]
+  (c/neg? (to-scalar x)))
 
-(defmethod zero? meajure.UnitValue [x]
-  (zero? (to-scalar x)))
+(defmethod c/zero? meajure.UnitValue [x]
+  (c/zero? (to-scalar x)))
 
-(defmethod = [meajure.UnitValue meajure.UnitValue] [x y]
+(defmethod c/= [meajure.UnitValue meajure.UnitValue] [x y]
   {:pre [(units-equal? x y)]}
-  (= (to-scalar x)
-     (to-scalar y)))
+  (c/= (to-scalar x)
+       (to-scalar y)))
 
-(defmethod > [meajure.UnitValue meajure.UnitValue] [x y]
+(defmethod c/> [meajure.UnitValue meajure.UnitValue] [x y]
   {:pre [(units-equal? x y)]}
-  (> (to-scalar x)
-     (to-scalar y)))
+  (c/> (to-scalar x)
+       (to-scalar y)))
 
-(defmethod < [meajure.UnitValue meajure.UnitValue] [x y]
+(defmethod c/< [meajure.UnitValue meajure.UnitValue] [x y]
   {:pre [(units-equal? x y)]}
-  (< (to-scalar x)
-     (to-scalar y)))
+  (c/< (to-scalar x)
+       (to-scalar y)))
 
-(defmethod >= [meajure.UnitValue meajure.UnitValue] [x y]
+(defmethod c/>= [meajure.UnitValue meajure.UnitValue] [x y]
   {:pre [(units-equal? x y)]}
-  (>= (to-scalar x)
-     (to-scalar y)))
+  (c/>= (to-scalar x)
+       (to-scalar y)))
 
-(defmethod <= [meajure.UnitValue meajure.UnitValue] [x y]
+(defmethod c/<= [meajure.UnitValue meajure.UnitValue] [x y]
   {:pre [(units-equal? x y)]}
-  (<= (to-scalar x)
-     (to-scalar y)))
+  (c/<= (to-scalar x)
+       (to-scalar y)))
