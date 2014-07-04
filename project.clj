aabc4ea2575fcd86a099ca39c6c6683662bc9d12
arrdem <me@arrdem.com>
2014-07-04 18:47:07 -0500

    Bump version for breaking change

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject me.arrdem/meajure "1.2.0"
+(defproject me.arrdem/meajure "2.0.0"
   :description "A Clojure math with units library"
   :url "http://github.com/arrdem/meajure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.generic "0.1.2"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/algo.generic "0.1.2"
+                  :exclusions [[org.clojure/clojure]]]])
